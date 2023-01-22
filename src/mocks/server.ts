02fc55ed8437707ef093677a7bafab5538dd8a2a
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-22 13:42:23 +0100

    improves server mock

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,27 +1,27 @@
 import { setupServer } from 'msw/node'
 import { rest } from 'msw'
 import { fakeTournaments } from '../utils/test-utils'
+const FAKE_DELAY = 500
 
 const handlers = [
   rest.get('http://localhost:4000/tournaments', (req, res, ctx) => {
     const searchedValue = req.url.searchParams.get('q')
-    const FAKE_DELAY = 500
-    if (searchedValue === 'Err') {
-      return res(ctx.status(500), ctx.json('🥸 Error'))
-    }
 
-    if (searchedValue === 'Load') {
-      return res(ctx.delay(FAKE_DELAY), ctx.json([]))
+    switch (searchedValue) {
+      case 'Err':
+        return res(ctx.status(500), ctx.json('🥸 Error'))
+      case 'Load':
+        return res(ctx.delay(FAKE_DELAY), ctx.json([]))
+      default:
+        if (searchedValue) {
+          const resultToShow = fakeTournaments.filter((t) =>
+            t.name.includes(searchedValue)
+          )
+          return res(ctx.json(resultToShow))
+        } else {
+          return res(ctx.json([]))
+        }
     }
-
-    if (searchedValue) {
-      const resultToShow = fakeTournaments.filter((t) =>
-        t.name.includes(searchedValue)
-      )
-      return res(ctx.json(resultToShow))
-    }
-
-    return res(ctx.json(fakeTournaments))
   }),
 ]
 
