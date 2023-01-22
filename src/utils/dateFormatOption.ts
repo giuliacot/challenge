c5460f09d51c5e84004076ee97d75c9b44de92ae
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-22 20:50:07 +0100

    adds constant utils

---
+++ src/utils/dateFormatOption.ts
@@ -0,0 +1,8 @@
+export const options: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+}
---
+++ src/utils/inputValidation.ts
@@ -0,0 +1,1 @@
+export const checkTournamentNameOnlyAllowedChars = new RegExp(/[a-zA-Z0-9\s]+$/)
