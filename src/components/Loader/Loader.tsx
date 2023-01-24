d105d3f9770e5b455f81fa9f045e1fa45c2ee621
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-24 11:19:41 +0100

    removes useless import

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { blink } from '../../common/animation'
-import theme from '../../common/theme'
 import { MainContent } from '../_layout/MainContent'
 
 const LoaderWrap = styled(MainContent)`
